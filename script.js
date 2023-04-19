3ca515ccc969b256d65ee0f99a7b24d1e07bf5bf
BIB <me@example.com>
2023-04-19 09:30:27 +0200

    script írása,

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-console.log("Script Loaded")
+function kiir3() {
+  console.log = ("a:", a)
+  console.log = ("b:", b)
+  console.log = ("c:", c)
+}
 
-const div1 = document.createElement("div")
+let div1 = document.createElement("div")
+let div2 = document.createElement("div")
+let div3 = document.createElement("div")
 
-console.log(div1)
+function kiirDiv3() {
+  console.log = ("div1:", div1)
+  console.log = ("div2:", div2)
+  console.log = ("div3:", div3)
+}
+div1.innerText = "Ez az első div"
+div2.innerText = "Ez a második div"
+div3.innerText = "Ez a harmadik div"
+
+let a = 1
+let b = 2
+let c = a
+kiir3()
+
+a = 7
+kiir3()
+document.body.append(div1, div2, div3)
+
+let tomb = [1, 22, 33, 14, 53]
+
+for (let i = 0; i < tomb.length; i++) {
+  console.error(tomb[i])
+}
+
+let tomb2 = tomb
